108fd148f96bb8ab439ee293fd3f309c621f32a3
hi1004 <kipo09@naver.com>
2023-02-27 20:27:47 +0900

    [props] props, defaultPorps

---
+++ react-beginner/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = ({ name }) => {
+  return <div>안녕하세요. 제 이름은 {name}입니다.</div>;
+};
+
+MyComponent.defaultProps = {
+  name: 'defaultName',
+};
+
+export default MyComponent;
