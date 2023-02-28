b87a78c44d8a9e80125a88dc6f88558c200b2fda
hi1004 <kipo09@naver.com>
2023-02-28 17:12:41 +0900

    [filter] 데이터 제거와 useState 활용

--- react-beginner/src/components/IterationSample.jsx
+++ react-beginner/src/components/IterationSample.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const fruits = ['apple', 'banana', 'orange', 'cherry'];
-  const fruitsList = fruits.map((fruits, index) => (
-    <li key={index}>{fruits}</li>
+  const [fruits, setFruits] = useState([
+    { id: 1, text: 'apple' },
+    { id: 2, text: 'banana' },
+    { id: 3, text: 'orange' },
+    { id: 4, text: 'cherry' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const handleChange = e => setInputText(e.target.value);
+  const handleClick = () => {
+    const nextFruits = [...fruits, { id: nextId, text: inputText }];
+    setNextId(nextId + 1);
+    setFruits(nextFruits);
+    setInputText('');
+  };
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') handleClick();
+  };
+  const handleRemove = id => {
+    const nextFruits = fruits.filter(fruit => fruit.id !== id);
+    setFruits(nextFruits);
+  };
+  const fruitsList = fruits.map(fruit => (
+    <li key={fruit.id} onDoubleClick={() => handleRemove(fruit.id)}>
+      {fruit.text}
+    </li>
   ));
-  return <ul>{fruitsList}</ul>;
+  return (
+    <>
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleClick}>추가</button>
+      <ul>{fruitsList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;
