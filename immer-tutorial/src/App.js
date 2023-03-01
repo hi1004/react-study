e98f332e346ae2de8e35600124d40e6b9b8711d3
hi1004 <kipo09@naver.com>
2023-03-01 23:46:43 +0900

    immer를 사용하지 않고 불변성 유지

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 const App = () => {
-  return <div></div>;
+  const [form, setForm] = useState({
+    username: '',
+    name: '',
+  });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+  const nextId = useRef(1);
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const { username, name } = form;
+      const info = {
+        id: nextId.current,
+        username,
+        name,
+      };
+      setData(data => ({
+        ...data,
+        array: data.array.concat(info),
+      }));
+
+      setForm({
+        username: '',
+        name: '',
+      });
+      nextId.current++;
+    },
+    [form]
+  );
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(form => ({
+      ...form,
+      [name]: [value],
+    }));
+  }, []);
+
+  const onRemove = useCallback(id => {
+    setData(data => ({
+      ...data,
+      array: data.array.filter(info => info.id !== id),
+    }));
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <ul>
+        {data.array.map(info => (
+          <li key={info.id} onClick={() => onRemove(info.id)}>
+            {info.username} ({info.name})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
