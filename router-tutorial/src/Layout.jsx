79fd4109de23f75394ce01366484882de823cfab
hi1004 <kipo09@naver.com>
2023-03-03 01:47:59 +0900

    [useNavigate]

--- router-tutorial/src/Layout.jsx
+++ router-tutorial/src/Layout.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    // 이전 페이지로 이동
+    navigate(-1);
+  };
+  const goArticles = () => {
+    // articles 경로로 이동
+    // replace 옵션 true, 페이지를 이동할때 현재 페이지를 페이지 기록에 남기지 않는다.
+    navigate('/articles', { replace: true });
+  };
   return (
     <div>
       <header
         style={{ backgroundColor: 'lightgray', padding: 16, fontSize: 24 }}
       >
-        Header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
