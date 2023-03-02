39e0265d46e7428b3520a4940e1d5f7527a82661
hi1004 <kipo09@naver.com>
2023-03-03 01:05:59 +0900

    [setSearchParams] 쿼리스트링-파싱

--- router-tutorial/src/pages/About.jsx
+++ router-tutorial/src/pages/About.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
+// https://localhost:3000/about?detail=true&mode=3
 const About = () => {
   const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get('detail');
+  const mode = searchParams.get('mode');
+
+  const onToggleDetail = () => {
+    setSearchParams({ detail: detail === 'true' ? false : true, mode });
+  };
+  const onIncreaseMode = () => {
+    const nextMode = mode === 'null' ? 1 : Number(mode) + 1;
+    setSearchParams({
+      detail: detail === 'null' ? true : false,
+      mode: nextMode,
+    });
+  };
 
   return (
     <div>
@@ -10,9 +25,10 @@ const About = () => {
       <p>about page</p>
       <p>현재 주소: {location.pathname}</p>
       <p>쿼리스트링: {location.search}</p>
-      <p>해시: {location.hash}</p>
-      <p>상태: {location.state}</p>
-      <p>key: {location.key}</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
     </div>
   );
 };
