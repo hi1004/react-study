86b1abc7fa5b31f81987547648d7527f48bfb52b
hi1004 <kipo09@naver.com>
2023-03-03 01:59:47 +0900

    NotFound 페이지

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -6,6 +6,7 @@ import Profile from './pages/Profile';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
 import Layout from './Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
