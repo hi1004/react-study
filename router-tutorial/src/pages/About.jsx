9f9345144e8424bea9090baa8ef1cc2acbad58b3
hi1004 <kipo09@naver.com>
2023-03-03 00:45:56 +0900

    [useLocation] 쿼리스트링

--- router-tutorial/src/pages/About.jsx
+++ router-tutorial/src/pages/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const About = () => {
+  const location = useLocation();
+
   return (
     <div>
       <h1>about</h1>
       <p>about page</p>
+      <p>현재 주소: {location.pathname}</p>
+      <p>쿼리스트링: {location.search}</p>
+      <p>해시: {location.hash}</p>
+      <p>상태: {location.state}</p>
+      <p>key: {location.key}</p>
     </div>
   );
 };
