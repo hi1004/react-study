1cbe80d82591778e2f249f656e0af9edbc002a8b
hi1004 <kipo09@naver.com>
2023-03-03 02:05:22 +0900

    [Navigate Component]

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -7,6 +7,8 @@ import Articles from './pages/Articles';
 import Article from './pages/Article';
 import Layout from './Layout';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
