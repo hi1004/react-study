9cf34d1d3eaa16cb27c71f49e40431458a9e5a52
hi1004 <kipo09@naver.com>
2023-03-02 22:37:07 +0900

    Link 컴포넌트를 사용하여 다른 페이지로 이동하는 링크 보여주기

--- router-tutorial/src/pages/Home.jsx
+++ router-tutorial/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>home</h1>
       <p>home page</p>
+      <Link to="/about">Go About Page</Link>
     </div>
   );
 };
