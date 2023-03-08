30e5efebf744f09752792a59f2ddf31a1eee9275
hi1004 <kipo09@naver.com>
2023-03-08 21:31:09 +0900

    Provider에 value 값 넣기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
 
+// Provider를 사용하면 Context의 value를 변경할 수 있다.
+// Provider를 사용할 때는 value 값을 명시해 주어야 한다.
 function App() {
   return (
     <div className="App">
