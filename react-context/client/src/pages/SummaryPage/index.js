6de64b9e18075f78073780498749602a1b3dab2c
hi1004 <kipo09@naver.com>
2023-04-28 18:58:16 +0900

    add SummaryPage

--- react-context/client/src/pages/SummaryPage/index.js
+++ react-context/client/src/pages/SummaryPage/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SummaryPage = () => {
-  return <div></div>;
+  const [checked, setChecked] = useState(false);
+  return (
+    <div>
+      <form>
+        <input
+          type="checkbox"
+          checked={checked}
+          id="confirm-checkbox"
+          onClick={e => setChecked(e.target.checked)}
+        />{' '}
+        <label for="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit" disabled={!checked}>
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SummaryPage;
