0ac47d043f8372c3c6bf2d96663a085ee20ab554
hi1004 <kipo09@naver.com>
2023-09-05 23:16:03 +0900

    About component styling

--- react-tailwind-app/src/components/About.jsx
+++ react-tailwind-app/src/components/About.jsx
@@ -1,5 +1,25 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section name="about" className="w-full h-screen">
+      <div className="flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto">
+        <div className="text-center">
+          <h2 className="text-5xl font-bold">About this company</h2>
+          <p className="py-6 text-3xl text-gray-500">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </div>
+
+        <p className="mt-20 text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          veritatis. Sequi nemo non fuga perferendis!
+        </p>
+        <p className="mt-20 text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          veritatis. Sequi nemo non fuga perferendis!
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
