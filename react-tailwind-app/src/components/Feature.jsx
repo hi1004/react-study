770283b6baaa568815ed12e680f594bf9bb3ab99
hi1004 <kipo09@naver.com>
2023-09-06 00:20:44 +0900

    Feature component styling

--- react-tailwind-app/src/components/Feature.jsx
+++ react-tailwind-app/src/components/Feature.jsx
@@ -1,5 +1,58 @@
+import { FaCheck } from 'react-icons/fa';
+
 const Feature = () => {
-  return <div>Feature</div>;
+  return (
+    <section name="feature" className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto px-2">
+        <h2 className="text-5xl font-bold text-center">Features</h2>
+        <p className="py-8 text-2xl text-center text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, dolore.
+        </p>
+        <div className="grid gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="flex">
+            <FaCheck className="mt-1 mr-3 text-blue-600 w-7" />
+            <div>
+              <h3 className="text-lg font-bold">Real-Time</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo,
+                tempora.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <FaCheck className="mt-1 mr-3 text-blue-600 w-7" />
+            <div>
+              <h3 className="text-lg font-bold">Real-Time</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo,
+                tempora.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <FaCheck className="mt-1 mr-3 text-blue-600 w-7" />
+            <div>
+              <h3 className="text-lg font-bold">Real-Time</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo,
+                tempora.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <FaCheck className="mt-1 mr-3 text-blue-600 w-7" />
+            <div>
+              <h3 className="text-lg font-bold">Real-Time</h3>
+              <p className="pt-2 pb-4 text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo,
+                tempora.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature;
